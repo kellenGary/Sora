acd96c2a4f9e75607a9d5606852378f33fbd579f
Emersonfras <emersonfrasure@gmail.com>
2025-10-13 20:29:45 -0400

    Functionality for Change Password Settings

--- app/src/main/java/com/example/sora/ui/settings/SettingScreen.kt
+++ app/src/main/java/com/example/sora/ui/settings/SettingScreen.kt
@@ -29,6 +29,7 @@ fun SettingScreen(navController: NavController? = null) {
 
         Spacer(modifier = Modifier.height(18.dp))
 
+        // Assuming SettingCard() and LanguageCard() are defined elsewhere
         SettingCard()
 
         Spacer(modifier = Modifier.height(30.dp))
